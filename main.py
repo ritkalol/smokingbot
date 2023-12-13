f8c9f767acdde8b6faa84c59d2974c31114ffa89
ritkalol <ritkalol@yandex.ru>
2023-12-13 23:20:26 +0300

    com1

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 import telebot
+import stuff
 
 print("[INFO] Bot started")
+
+with open("tokfile", 'r') as tf:
+    token = tf.readlines()[0].replace('\n', '')
+
+bot = telebot.TeleBot(token)
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(
+        message.chat.id, stuff.start_prompt)
+
+
+bot.infinity_polling()
